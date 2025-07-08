9b2dbf0e514a650508c8692613c8d17967cb9bea
fabry76 <fabrizio.fabiani@gmail.com>
2025-07-08 21:57:09 +0200

    new

--- debian_gnome.sh
+++ debian_gnome.sh
@@ -15,7 +15,7 @@ echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://package
 apt update && apt install firefox -y
 
 # desktop environment
-apt install gnome-core gnome-shell-extension-dash-to-panel gnome-weather gnome-calendar gnome-clocks gnome-tweaks file-roller seahorse transmission-gtk shotwell -y
+apt install gnome-core gnome-themes-extra gnome-shell-extension-dash-to-panel gnome-weather gnome-calendar gnome-clocks gnome-tweaks file-roller seahorse transmission-gtk shotwell -y
 
 # apps & utilities
 apt install timeshift vim htop fastfetch unrar net-tools curl apt-file plymouth-themes apt-transport-https dracut-core apt-show-versions debsums distrobox -y
