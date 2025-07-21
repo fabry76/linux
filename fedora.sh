e1bdd5f1a2fba6aa06067e539c3c3e96f22f19be
fabry76 <fabrizio.fabiani@gmail.com>
2025-07-21 15:21:24 +0200

    new

--- fedora.sh
+++ fedora.sh
@@ -3,6 +3,7 @@ dnf update -y
 hostnamectl set-hostname FEDORA
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.mattjakeman.ExtensionManager
 
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
