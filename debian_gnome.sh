2682ae57ae5bb100e704637fa17403e232972bfe
fabry76 <fabrizio.fabiani@gmail.com>
2025-08-17 22:33:14 +0200

    new

--- debian_gnome.sh
+++ debian_gnome.sh
@@ -38,7 +38,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -f ./google-chrome-stable_current_amd64.deb -y
 
 # firewall and network
-apt install gufw avahi-daemon -y
+apt install ufw avahi-daemon -y
 sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
 ufw enable
 ufw allow mdns
