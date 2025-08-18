39a5636e27641cf0bf7b5a01b87a52db0f7a1f87
fabry76 <fabrizio.fabiani@gmail.com>
2025-08-18 13:58:56 +0200

    new

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 # install applications
-apt install ubuntu-restricted-extras showtime ffmpeg gnome-shell-extension-manager gnome-weather gnome-calendar gnome-tweaks gnome-snapshot ffmpegthumbnailer timeshift fastfetch curl htop net-tools apt-transport-https vim transmission dracut-core apt-show-versions debsums -y
+apt install ubuntu-restricted-extras showtime ffmpeg gnome-shell-extension-manager gnome-weather gnome-calendar gnome-tweaks gnome-snapshot gnome-sound-recorder ffmpegthumbnailer timeshift fastfetch curl htop net-tools apt-transport-https vim transmission dracut-core apt-show-versions debsums -y
 
 # vlc vlc-plugin-pipewire 
 
