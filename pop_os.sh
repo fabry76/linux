83918b285ece2a27daeb6c280e4f6976c8c34a26
fabry76 <fabrizio.fabiani@gmail.com>
2025-11-21 21:39:58 +0100

    new

---
+++ pop_os.sh
@@ -0,0 +1,31 @@
+# purge apps
+apt purge libreoffice* thunderbird -y
+apt autoremove -y
+
+# update repositories
+apt update && apt upgrade -y
+
+# brave
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# install applications
+apt install ubuntu-restricted-extras intel-media-va-driver-non-free timeshift curl htop vim fwupd code transmission -y
+
+# flatpaks
+flatpak install flathub org.onlyoffice.desktopeditors -y
+
+# printing and scanning
+apt install printer-driver-all printer-driver-cups-pdf -y
+adduser fabri lpadmin
+
+# firewall
+ufw enable
+ufw allow mdns
+
+# fastgate
+apt install cifs-utils smbclient -y
+tee -a /etc/fstab  << END
+# map fastgate usb storage
+//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
+END
+
