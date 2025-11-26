392cadf6c59aafbc2922689a8fbe259cc81ff85c
fabry76 <fabrizio.fabiani@gmail.com>
2025-11-26 20:33:04 +0100

    new

---
+++ mint.sh
@@ -0,0 +1,30 @@
+# purge apps
+apt purge libreoffice* -y
+apt autoremove -y
+
+# update repositories
+apt update && apt upgrade -y
+
+# brave
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# snaps
+rm -rf /etc/apt/preferences.d/nosnap.pref
+
+# install applications
+apt install snapd ubuntu-restricted-extras intel-media-va-driver-non-free timeshift curl htop vim fwupd code transmission -y
+
+# printing and scanning
+apt install printer-driver-all printer-driver-cups-pdf -y
+adduser fabri lpadmin
+
+# firewall
+ufw enable
+ufw allow mdns
+
+# fastgate
+apt install cifs-utils smbclient -y
+tee -a /etc/fstab  << END
+# map fastgate usb storage
+//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
+END
