44944ccc91e2f777bf4bb0d8c998bcbc847f5e72
fabry76 <fabrizio.fabiani@gmail.com>
2025-12-02 22:32:37 +0100

    new

--- arch2_cosmic.sh
+++ arch2_cosmic.sh
@@ -11,7 +11,7 @@ systemctl enable cronie
 usermod --add-subuids 100000-165535 --add-subgids 100000-165535 fabri
 
 # utilities
-pacman -S --needed speech-dispatcher curl fastfetch less rust wget bash-completion sof-firmware appstream plocate unrar unzip 7zip fuse2 ffmpeg ffmpegthumbs ffmpegthumbnailer gst-libav gst-plugins-ugly dnsutils whois --noconfirm
+pacman -S --needed gvfs gvfs-smb speech-dispatcher curl fastfetch less rust wget bash-completion sof-firmware plocate unrar unzip 7zip fuse2 ffmpeg ffmpegthumbs ffmpegthumbnailer gst-libav gst-plugins-ugly dnsutils whois --noconfirm
 
 # fonts
 pacman -S --needed ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-dejavu noto-fonts adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts nerd-fonts --noconfirm
