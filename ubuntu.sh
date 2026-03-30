30a2a9de73bc9ccbc381343023d97fb4e0810c23
fabry76 <fabrizio.fabiani@gmail.com>
2026-03-30 14:35:09 +0200

    new

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,12 +1,5 @@
 # install applications
-apt install ubuntu-restricted-extras intel-media-va-driver-non-free showtime ffmpeg gnome-shell-extension-manager gnome-weather gnome-calendar gnome-tweaks gnome-snapshot gnome-sound-recorder ffmpegthumbnailer timeshift fastfetch curl htop net-tools apt-transport-https vim apt-show-versions fwupd -y
-
-# cockpit
-apt install cockpit cockpit-machines virt-viewer -y
-systemctl enable cockpit.socket
-
-# brave
-curl -fsS https://dl.brave.com/install.sh | sh
+apt install ubuntu-restricted-extras intel-media-va-driver-non-free showtime ffmpeg gnome-shell-extension-manager gnome-weather gnome-calendar gnome-tweaks gnome-snapshot gnome-sound-recorder gnome-boxes ffmpegthumbnailer timeshift fastfetch curl htop net-tools apt-transport-https vim apt-show-versions fwupd -y
 
 # snaps
 snap install pinta onlyoffice-desktopeditors transmission
@@ -32,13 +25,13 @@ sed -i 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 
 # grub
-sed -i 's/quiet/quiet loglevel=3/g' /etc/default/grub
-tee -a /etc/default/grub << END
-GRUB_RECORDFAIL_TIMEOUT=5
-END
-update-grub
+# sed -i 's/quiet/quiet loglevel=3/g' /etc/default/grub
+# tee -a /etc/default/grub << END
+# GRUB_RECORDFAIL_TIMEOUT=5
+# END
+# update-grub
 
 # varie
-sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/g' /etc/systemd/logind.conf
-sed -i 's/#HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore/g' /etc/systemd/logind.conf
-fc-cache -fv
+# sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/g' /etc/systemd/logind.conf
+# sed -i 's/#HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore/g' /etc/systemd/logind.conf
+# fc-cache -fv
