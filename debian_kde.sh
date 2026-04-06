f93e99ea5bd686ccba7c2dfd71baa9d11b09328d
fabry76 <fabrizio.fabiani@gmail.com>
2026-04-06 10:56:17 +0200

    new

--- debian_kde.sh
+++ debian_kde.sh
@@ -11,7 +11,7 @@ apt install firmware-linux firmware-sof-signed firmware-realtek -y
 # desktop environment
 apt install kde-plasma-desktop ark kalk kde-spectacle ksystemlog isoimagewriter transmission-qt kolourpaint kamoso gwenview okular -y
 
-F# vscode
+# vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
