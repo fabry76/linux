decb8cdc46650e328b61cc8212e015a51e2c0d93
fabry76 <fabrizio.fabiani@gmail.com>
2026-05-03 10:57:18 +0200

    new

--- gdrive_sync.sh
+++ gdrive_sync.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
 LOG="$HOME/rclone-upload.log"
-rclone sync "$HOME/Documents" gdrive: -P -v --fast-list 2>&1 | tee "$LOG"
+
+rclone sync "$HOME/Documents" gdrive: \
+  -P -v \
+  --fast-list \
+  2>&1 | tee "$LOG"
+
 read -p "Sync completed. Press Enter to close..."
+
