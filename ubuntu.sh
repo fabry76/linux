1aba907a704e37bffe181a1e0b96585518ffb601
fabry76 <fabrizio.fabiani@gmail.com>
2026-05-09 19:35:49 +0200

    new

--- ubuntu.sh
+++ ubuntu.sh
@@ -15,3 +15,23 @@ apt install printer-driver-all printer-driver-cups-pdf sane simple-scan -y
 # locale
 sed -i 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
+
+###############################################
+# Optional Fastgate setup
+###############################################
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ -f "$SCRIPT_DIR/2_fastgate.sh" ]; then
+  echo
+  read -rp "Mount Fastgate now? (y/N): " RUN_FASTGATE
+
+  if [[ "$RUN_FASTGATE" =~ ^[Yy]$ ]]; then
+    bash "$SCRIPT_DIR/2_fastgate.sh"
+  fi
+fi
+
+###############################################
+# Cleanup
+###############################################
+apt-get -y autoremove
+apt-get clean
