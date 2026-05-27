7886971704a87877838032bd8a3406ff8c9fc25b
Fabrizio <fabri@L16G2.lan>
2026-05-27 21:25:55 +0200

    new

--- 1_debian.sh
+++ 1_debian.sh
@@ -142,7 +142,29 @@ apt-get install -y firmware-misc-nonfree linux-headers-amd64 firmware-sof-signed
 ###############################################
 # KDE Plasma
 ###############################################
-apt-get install -y kde-plasma-desktop konsole ark kalk isoimagewriter kolourpaint gwenview okular okular-extra-backends kcharselect kcolorchooser filelight plasma-widgets-addons krecorder plasma-workspace-wallpapers
+###############################################
+# KDE Plasma
+###############################################
+apt-get install -y --no-install-recommends \
+kde-plasma-desktop \
+sddm \
+plasma-nm \
+dolphin \
+kate \
+konsole \
+ark \
+kalk \
+isoimagewriter \
+kolourpaint \
+gwenview \
+okular \
+okular-extra-backends \
+kcharselect \
+kcolorchooser \
+filelight \
+plasma-widgets-addons \
+krecorder \
+plasma-workspace-wallpapers
 
 ###############################################
 # KDE Flatpak
