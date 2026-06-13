ceec5d4b43a6a07a0e29c148dec5e87f7dc1a22a
fabry76 <fabrizio.fabiani@gmail.com>
2026-06-13 12:21:12 +0200

    new

--- rpm/brave.sh
+++ rpm/brave.sh
@@ -1,3 +1,3 @@
 dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
-dnf check-update && dnf install -y brave-origin
+dnf install -y brave-origin
