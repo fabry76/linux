0033f7a91eb7584b3da042a6cc4b406c934ca94a
fabry76 <fabrizio.fabiani@gmail.com>
2026-06-13 12:10:15 +0200

    new

--- rpm/fedora.sh
+++ rpm/fedora.sh
@@ -183,103 +183,10 @@ dnf-plugins-core \
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-###############################################
-# Gnome
-###############################################
-dnf install -y @gnome-desktop \
-    --setopt=install_weak_deps=False \
-    --exclude=gnome-boxes \
-    --exclude=gnome-maps
-
-systemctl enable gdm
-systemctl set-default graphical.target
-
-dnf install -y \
-    gnome-tweaks \
-    gnome-extensions-app \
-    gnome-shell-extension-dash-to-panel
-
-###############################################
-# Flatpak
-###############################################
-flatpak remote-add --if-not-exists \
-    flathub \
-    https://dl.flathub.org/repo/flathub.flatpakrepo
-
-###############################################
-# Variables
-###############################################
-BROWSER_APP=""
-OFFICE_APP=""
-
-FLATPAK_APPS=(
-    com.transmissionbt.Transmission
-    com.github.tchx84.Flatseal
-)
-
-###############################################
-# Browser selection
-###############################################
-case "$FLATPAK_BROWSER" in
-    1)
-        BROWSER_APP="org.mozilla.firefox"
-        FLATPAK_APPS+=(org.mozilla.firefox)
-        ;;
-    2)
-        BROWSER_APP="com.brave.Browser"
-        FLATPAK_APPS+=(com.brave.Browser)
-        ;;
-    3)
-        BROWSER_APP="io.gitlab.librewolf-community"
-        FLATPAK_APPS+=(io.gitlab.librewolf-community)
-        ;;
-    0)
-        ;;
-esac
-
-###############################################
-# Office selection
-###############################################
-case "$OFFICE_CHOICE" in
-    1)
-        OFFICE_APP="org.onlyoffice.desktopeditors"
-        FLATPAK_APPS+=(org.onlyoffice.desktopeditors)
-        ;;
-    2)
-        OFFICE_APP="org.libreoffice.LibreOffice"
-        FLATPAK_APPS+=(org.libreoffice.LibreOffice)
-        ;;
-    3)
-        OFFICE_APP="com.collaboraoffice.Office"
-        FLATPAK_APPS+=(com.collaboraoffice.Office)
-        ;;
-    0)
-        ;;
-esac
-
-###############################################
-# Install Flatpaks
-###############################################
-flatpak install -y --system flathub "${FLATPAK_APPS[@]}"
-
-###############################################
-# Gnome apps override
-###############################################
-runuser -u "$TARGET_USER" -- bash -c \
-    "flatpak override --user com.transmissionbt.Transmission --nofilesystem=host --filesystem=xdg-download"
-
-###############################################
-# Browser override (dynamic)
-###############################################
-if [ -n "$BROWSER_APP" ]; then
-    runuser -u "$TARGET_USER" -- bash -c "
-        flatpak override --user $BROWSER_APP \
-        --nofilesystem=host \
-        --filesystem=xdg-download \
-        --nodevice=all \
-        --nosocket=x11
-    "
-fi
+################################################
+# Desktop Environment
+################################################
+bash "$SCRIPT_DIR/gnome.sh" "$TARGET_USER" "$FLATPAK_BROWSER" "$OFFICE_CHOICE"
 
 ###############################################
 # Browsers
@@ -358,6 +265,24 @@ dnf install -y \
 dnf install -y cups gutenprint cups-pdf
 systemctl enable cups
 usermod -aG sys "$TARGET_USER"
+
+###############################################
+# Virtualization
+###############################################
+###############################################
+# Cockpit + Virtual Machines
+###############################################
+dnf install -y cockpit cockpit-machines qemu-kvm libvirt virt-install
+systemctl enable cockpit.socket
+systemctl enable libvirtd
+
+###############################################
+# Firewall
+###############################################
+dnf install -y firewalld firewall-config
+systemctl enable firewalld
+firewall-cmd --permanent --add-service=mdns
+firewall-cmd --reload
 
 ###############################################
 # Fastgate
