79b93ee4ee8d783417571ae69360f41ae72609b3
fabry76 <fabrizio.fabiani@gmail.com>
2026-06-13 14:02:32 +0200

    new

--- rpm/hardening.sh
+++ rpm/hardening.sh
@@ -10,6 +10,11 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 ###############################################
+# Disable SSH
+###############################################
+systemctl disable sshd
+
+###############################################
 # Function
 ###############################################
 write_if_changed() {
@@ -27,29 +32,10 @@ write_if_changed() {
 # Kernel Hardening
 ###############################################
 write_if_changed /etc/sysctl.d/99-hardening.conf "$(cat << 'EOF'
-kernel.kptr_restrict = 2
-kernel.sysrq = 0
-kernel.dmesg_restrict = 1
-kernel.kexec_load_disabled = 1
-kernel.yama.ptrace_scope = 1
-kernel.unprivileged_bpf_disabled = 1
-
-net.core.bpf_jit_harden = 2
-
-net.ipv4.conf.all.rp_filter = 2
-net.ipv4.conf.default.rp_filter = 2
-
-net.ipv4.conf.all.send_redirects = 0
-net.ipv4.conf.default.send_redirects = 0
-
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv4.conf.default.accept_redirects = 0
-
-fs.protected_fifos = 2
-fs.protected_regular = 2
-fs.protected_symlinks = 1
+kernel.randomize_va_space = 2
+net.ipv4.tcp_syncookies = 1
 fs.protected_hardlinks = 1
-fs.suid_dumpable = 0
+fs.protected_symlinks = 1
 EOF
 )"
 
