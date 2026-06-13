755aec7137aad152c2a2ceb70bb4df23c6fe8b6e
fabry76 <fabrizio.fabiani@gmail.com>
2026-06-13 14:05:32 +0200

    new

--- rpm/brave.sh
+++ rpm/brave.sh
@@ -1,3 +1,2 @@
 dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-
 dnf install -y brave-origin
---
+++ rpm/chrome.sh
@@ -0,0 +1,2 @@
+dnf config-manager addrepo --from-repofile=https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome.repo
+dnf install -y google-chrome-stable
