790f48166547e761b7f40fbb73d9380cbff639a0
fabry76 <fabrizio.fabiani@gmail.com>
2026-06-14 18:22:00 +0200

    new

--- deb/cockpit_vm.sh
+++ deb/cockpit_vm.sh
@@ -3,7 +3,16 @@ set -euo pipefail
 
 TARGET_USER="$1"
 
-apt-get install -y cockpit-machines qemu-kvm libvirt-daemon-system virtinst libvirt-clients bridge-utils virt-viewer
+apt-get install -y \
+cockpit-machines \
+qemu-kvm \
+libvirt-daemon-system \
+virtinst \
+libvirt-clients \
+bridge-utils \
+virt-viewer \
+dnsmasq
+
 usermod -aG libvirt,kvm "$TARGET_USER"
 sudo systemctl enable libvirtd
 
