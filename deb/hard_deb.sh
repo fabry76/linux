22ec708940f41a1db86c8a24dc93d700558f91ae
fabry76 <fabrizio.fabiani@gmail.com>
2026-08-14 23:14:31 +0200

    new

--- deb/hard_deb.sh
+++ deb/hard_deb.sh
@@ -53,6 +53,9 @@ net.core.bpf_jit_harden = 2
 
 net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.all.log_martians = 1
+net.ipv4.conf.default.log_martians = 1
+net.ipv4.conf.all.rp_filter = 1
 EOF
 )"
 
